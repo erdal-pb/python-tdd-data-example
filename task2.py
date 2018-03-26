3c0da8b9c0950b1c705e88c9d61469060d52718c
dgokeeffe <davidok7@gmail.com>
2018-03-26 17:04:24 +1100

    Collect Crash Data

---
+++ task2.py
@@ -0,0 +1,74 @@
+#!/home/kazooy/anaconda3/bin/python
+'''
+author: David O'Keeffe
+date: 23/3/2018
+
+task_2
+~~~~~
+
+Steps:
+Write the code to perform an ETL process to extract a data set from the supplied source
+Persist outputs and Visualise the data in an accessible format
+
+http://www.planecrashinfo.com/database.htm
+
+
+Output:
+    Total fatalities between period 1920-2016 period
+    Top 3 airlines with the highest rate of incidents
+    Year with the highest incidents
+
+
+'''
+
+import luigi
+import csv
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import pandas as pd
+import seaborn as sns
+
+
+class GetPlaneData(luigi.Task):
+    crash_array = [] 
+
+    def output(self):
+        return crash_array
+
+    def run(self):
+        # Get the URL
+        url = "http://www.planecrashinfo.com/database.htm"
+        page = urlopen(url)
+        soup = BeautifulSoup(page, "lxml")
+
+        # Pull all the a tags inside of table cells
+        years = soup.select('tr a')
+        domain = "http://www.planecrashinfo.com"
+        year_links = [domain + link.get('href') for link in years]
+
+        # For each year fetch all the links then open each record
+        for year in year_links:
+            page = urlopen(year)
+            soup = BeautifulSoup(page, "lxml")
+            crashes = soup.select('tr a')
+            crash_links = [year[:-8] + link.get('href') for link in crashes]
+
+            for crash in crash_links:
+                page = urlopen(crash)
+                soup = BeautifulSoup(page, "lxml")
+                data = soup.select('tr font')
+
+                # Based off the table structure collect every odd index
+                crash_temp = data[3::2]
+                crash_row = [crash] + [row.get_text() for row in crash_temp]
+                crash_array.append(crash_row)
+
+        # Initialize CSV
+        with open("crash_data.csv", "w+") as my_csv:
+            csvWriter = csv.writer(my_csv, delimiter=',')
+            csvWriter.writerows(crash_array)
+             
+
+if __name__ == "__main__":
+    luigi.run()
+
